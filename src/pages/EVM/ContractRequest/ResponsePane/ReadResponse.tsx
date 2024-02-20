bc62cca3404321457feb8710a4b16f3893670383
Hieu Do <hieudm@yunero.io>
2024-02-20 16:44:46 +0700

    feat: handle render result

--- src/pages/EVM/ContractRequest/ResponsePane/ReadResponse.tsx
+++ src/pages/EVM/ContractRequest/ResponsePane/ReadResponse.tsx
@@ -2,22 +2,26 @@ import { Response } from '@/store/responses'
 import ReactJson from 'react-json-view'
 import { useState, useEffect } from 'react'
 export default function ReadResponse({ response }: { response: Response }) {
-  const [responseDisplay, setResponseDisplay] = useState();
+  const [responseDisplay, setResponseDisplay] = useState()
   useEffect(() => {
-    let jsonResult:any = {};
     if (response.result) {
-      for (let i = 0; i < response.abi.outputs.length; i++) {
-        jsonResult[response.abi.outputs[i].name ? response.abi.outputs[i].name : response.abi.outputs[i].type] = response.result[i];
+      console.log(typeof response.result, response.result)
+      if (typeof response.result == 'number' || typeof response.result == 'bigint') {
+        response.result = {
+          value: response.result.toString(),
+        }
       }
+      setResponseDisplay(response.result)
     }
-    setResponseDisplay(jsonResult);
-  }, [response]);
+  }, [response])
   return (
     <div className="hover:bg-muted/60 p-2 rounded">
       <p className="text-primary">
         CALLED TO {response.functionName} at [ChainID={response.chainId} {response.address}]
       </p>
-      <pre className="whitespace-pre"><ReactJson src={responseDisplay ? responseDisplay: {}} /></pre>
+      <pre className="whitespace-pre">
+        <ReactJson src={responseDisplay ? responseDisplay : {}} />
+      </pre>
     </div>
   )
 }
